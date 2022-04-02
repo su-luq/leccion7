4d5c28f288ce880723bc233b4e2045c86b135bb1
Marceli Subirana <msubiw@gmail.com>
2022-04-02 13:14:02 +0200

    update

--- destructuring.js
+++ destructuring.js
@@ -35,3 +35,26 @@ console.log('--> ', resto, ' ## ', resto2); // resto2 es el resto dentro de dire
 const adressa = resto2; // li canviao el nom xq vull que a la següent estructura sigui adressa i no resto2
 const total = { ...resto, adressa }; //fusionar dos objectes
 console.log('Total-calle: ', total); // undefined
+
+// arrays
+var arrayObject = ["Juan", "Perez", 23];
+const [nombre2, apellido2, edad2] = arrayObject; // assignem els valors de l'array a les variables.
+console.log(nombre2, apellido2, edad2); // Juan Perez 23
+for (const valor of arrayObject) {
+    console.log("valor de l'array: ", valor);
+}
+console.log(arrayObject[0]); // Juan
+
+// en un array puedo tener objetos i deiferentes tipos.
+
+var v1 = [1,2,3,4,new Date()];
+const [primero, ...restoArray] = v1;    
+console.log(primero); // 1
+console.log(restoArray); // [2,3,4,new Date()]
+
+//unir arrays con el operador ...
+
+v2 = [6,7,8];
+v3 = [...v1, ...v2];
+console.log (v1," # ", v2, " ## ", v3); // [1,2,3,4,new Date(), 6,7,8]
+
