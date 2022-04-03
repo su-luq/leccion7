97c2b362761c69008ab186cc78f92cb2798e82ad
Marceli Subirana <msubiw@gmail.com>
2022-04-03 09:41:00 +0200

    update per context

--- curso-react-25-03/src/main.jsx
+++ curso-react-25-03/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* <App2 /> {/* aquest es tot excepte contect que ho poso a un nou App */}
     <App />
   </React.StrictMode>,
   document.getElementById('rootCurso') // per defecte surt root i ho hem canviat per veure que a l'Index, tamble caldrà posar el mateix nom.
