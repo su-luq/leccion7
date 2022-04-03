ba44d2591d0eb772d98a822537996cdba5eeb265
Marceli Subirana <msubiw@gmail.com>
2022-04-03 09:56:13 +0200

    update

--- curso-react-25-03/src/App.jsx
+++ curso-react-25-03/src/App.jsx
@@ -1,12 +1,7 @@
+// Exclusiu per Ract.context
 import './subi.css'
-import { useState } from 'react'
+import { useState, createContext } from 'react'
 
-function Hijo(prop1) {
-  return <div>
-    El nom del fill és {prop1.nom}
-    <Nieto nom="none"/> {/*comentario en jsx entre llaves */}
-  </div>
-}
 
 function Nieto({nom}) {
   return <div>
@@ -14,88 +9,28 @@ function Nieto({nom}) {
   </div>
 }
 
-// a Hija, passem només el valor "destructiring"
-
-function Hija({nom}) {
-  nom += " Subirana" // puc modificar la propietat, però no el valor de la crida de la funció.
+function Hijo(nom) {
   return <div>
-    El nom de la filla és {nom}
+    Hijo: {nom}
+    <Nieto nom="none"/> 
   </div>
 }
 
-function Filla_2({nom, saldo}) {
-  return <div>
-    El nom de la filla és {nom} i el saldo és {saldo}
-  </div>
-}
-
-function Fill_2(param) {
-  return <div>
-    El nom del fill és {param.nom} i el saldo és {param.saldo}
-  </div>
-}
-
-//Anem a fer que puguem canviar el saldo.
-function Elf({nom, saldo}) {
-  return <div>
-    El nom és {nom} i el saldo és {saldo} 
-    <br></br>
-    <button onClick={() => { cambiarSaldo(saldo) }}>Cambiar saldo</button>
-  </div>
-}
-
-function cambiarSaldo(saldo) {
-  return saldo += 100
-}
-
-// para canviar realmente un valor, debemos meternos en la gestión del estado.
-function ElfSaldo({nom, saldo}) {
-  const [saldoElf, setSaldoElf] = useState(saldo) // saldoElf, nova variablem setSaldoElf funció i useState inicialització de valor inicial.
-
-  function cambiarSaldoElf () {
-    setSaldoElf(saldoElf + 10)
-  }
-  
-  return <div>
-    El nom és {nom} i el saldo és {saldoElf} 
-    <br></br>
-    <button onClick={() => { cambiarSaldoElf() }}>Cambiar saldo</button>
-  </div>
-}
-
-// però com actualitzem el saldo d'una variable global?
-
-// Primer intent, passant la funció de canvi.
-function ElfSaldo2({nom, saldo, setSaldoElf: setSaldoNew}) {
-  const [saldoElf, setSaldoElf] = useState(saldo) // saldoElf, nova variablem setSaldoElf funció i useState inicialització de valor inicial.
-
-  function cambiarSaldoElf () {
-    setSaldoElf(saldoElf + 10)
-    setSaldoNew(saldoElf + 10)
-  }
-  
-  return <div>
-    Passant la funció de set. El nom és {nom} i el saldo és {saldoElf} 
-    <br></br>
-    <button onClick={() => { cambiarSaldoElf() }}>Cambiar saldo</button>
-  </div>
-}
+const Contexto = React.createContext()
 
 function App() {
-  const [saldoElfMain, setSaldoElfMain] = useState(1000)
-  
+  const [estado, setEstado] = useState({
+    saldo: 100
+  })
+  const value = useMemo((estado) => ({estado, setEstado}), [estado])
+
   return(
-    <div className="App">
-      <p>Hello world</p>
-      <Hijo nom="Òscar" />
-      <Hija nom="Sara" />
-      <Filla_2 nom="Sara" saldo="100" />
-      <Fill_2 nom="Òscar" saldo="200" />
-      <Elf nom="Encarni" saldo="300" />
-      <p>Encarni {saldoElfMain}</p>
-      <ElfSaldo nom="Encarni" saldo={saldoElfMain} />
-      <ElfSaldo2 nom="Encarni" saldo={saldoElfMain} setSaldoElf={setSaldoElfMain} />
-    </div>
+    <Contexto.Provider value={estado}>
+      <div className="App">
+        <p>Encarnita {saldo}</p>
+        <Hijo nom="Òscar" />
+      </div>
+    </Contexto.Provider>
   )
 }
 
